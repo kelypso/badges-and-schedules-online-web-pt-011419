62fa4d70e3e671735d36fef7db0db3b3cffc3fe7
Kelsey Low <kklowlow@gmail.com>
2019-01-21 18:20:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  badges << badge_maker(attendees)
+  return badges
+end
