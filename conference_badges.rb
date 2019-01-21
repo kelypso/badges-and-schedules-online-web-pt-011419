a331434f4357657e543561b593df3d9b4d2a7e16
Kelsey Low <kklowlow@gmail.com>
2019-01-21 18:35:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,11 @@ def  badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  
+  badges = []
+  attendees.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
 end
 
 def assign_rooms(attendees)
